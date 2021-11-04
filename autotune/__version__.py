79f816a8f130ff61ef32faa2de438078487565b7
Brice Videau <bvideau@anl.gov>
2021-11-04 11:52:13 -0500

    Renamed package to ytopt-autotune and changed version number.

--- autotune/__version__.py
+++ autotune/__version__.py
@@ -1,6 +1,6 @@
-VERSION = (0, 0, 1)
+VERSION = (1, 1, 0)
 
 __version__ = '.'.join(map(str, VERSION))
 
 # alpha/beta/rc tags
-__version_suffix__ = 'alpha0'
+#__version_suffix__ = 'alpha0'
--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 import os
 import io
 
-NAME = 'autotune'
+NAME = 'ytopt-autotune'
+DIRNAME = 'autotune'
 DESCRIPTION = ''
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Load the package's __version__.py module as a dictionary.
 about = dict()
-with open(os.path.join(here, NAME, '__version__.py')) as f:
+with open(os.path.join(here, DIRNAME, '__version__.py')) as f:
     exec(f.read(), about)
 
 # Import the README and use it as the long-description.
@@ -33,7 +34,7 @@ setup(
   name=NAME,
   version=about['__version__'],
   description=DESCRIPTION,
-  packages=['autotune'],
+  packages=[DIRNAME],
   install_requires=REQUIRED,
   extras_require=EXTRAS,
 )
