a09608ed1943389ec5e218c328c7987d9880a23e
Xingfu Wu <38541094+wuxf99@users.noreply.github.com>
2022-06-09 19:34:57 -0500

    Update run.bat

--- Benchmarks/ECP-Apps/XSBench-Laptop/xsbench-mpi/xsbench/run.bat
+++ Benchmarks/ECP-Apps/XSBench-Laptop/xsbench-mpi/xsbench/run.bat
@@ -4,5 +4,5 @@ let nnds=1
 #--- process processexe.pl to change the number of MPI ranks
 ./processcp.pl ${nnds}
 
-python -m ytopt.search.ambs --evaluator ray --problem problem.Problem --max-evals=3 --learner RF
+python -m ytopt.search.ambs --evaluator ray --problem problem.Problem --max-evals=5 --learner RF
 
